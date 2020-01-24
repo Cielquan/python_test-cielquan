cbddc8f35330adf6543e6edc99efdac95cb39245
Cielquan <cielquan@protonmail.com>
2020-01-24 22:55:28 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,76 +1,97 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+# ======================================================================================
+# Copyright (c) 2020 Christian Riedel
 #
+# This file 'conf.py' created 2020-01-24 is part of the project/program 'DoTH-DNS'.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published by
+# the Massachusetts Institute of Technology.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MIT License for more details.
+#
+# You should have received a copy of the MIT License
+# along with this program. If not, see <https://opensource.org/licenses/MIT>.
+#
+# Github: https://github.com/Cielquan/
+# ======================================================================================
+"""
+    docs.source.conf
+    ~~~~~~~~~~~~~~~~
+
+    Configuration file for the Sphinx documentation builder.
+
+    :copyright: 2020 (c) Christian Riedel
+    :license: MIT, see LICENSE.rst for more details
+"""
 import os
-import sys
 from pathlib import Path
+import sys
 
+import dothdns as project_info
+
+
+# Paths
 sys.path.insert(0, os.path.abspath("../.."))
 conf_dir = Path(__file__)
-# from python_test import __version__
 
 
-# -- Project information -----------------------------------------------------
+# -- PROJECT INFORMATION ---------------------------------------------------------------
 
-project = "python_test"
-copyright = "2020, Cielquan"
-author = "Cielquan"
-
+project = project_info.__title__
+author = project_info.__author__
+copyright = "2020, " + author
 # The full version, including alpha/beta/rc tags
-version = __version__ = "0"
-release = version
-# release_date = ""
-#
-# rst_epilog = """
-# .. |release_date| replace:: {release_date}
-# .. |coverage-equals-release| replace:: coverage=={release}
-# .. |doc-url| replace:: https://coverage.readthedocs.io/en/coverage-{release}
-# .. |br| raw:: html
-#   <br/>
-# """.format(release=release, release_date=release_date)
+release = ".".join(project_info.__version_info__[0:2])
+# Major version like (X.Y)
+version = project_info.__version__
+# Release date
+release_date = "2020"  #: CHANGEME
 
 
-# -- General configuration ---------------------------------------------------
+# -- SPHINX CONFIG ---------------------------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings.
 extensions = [
     "sphinx.ext.autodoc",
 ]
-
-master_doc = "index"
-
-pygments_style = 'sphinx'
 
 # intersphinx_mapping = {
 #     'python': ('https://docs.python.org/3', None),
 #     }
 
-# Add any paths that contain templates here, relative to this directory.
+
+# -- FILES -----------------------------------------------------------------------------
+
+# Index source file
+master_doc = "index"
+
+# Files to exclude for source of doc
+exclude_patterns = []
+
+# Folder for static files, if folder exists
+html_static_path = []
+if Path(conf_dir, "_static").exists():
+    html_static_path = ["_static"]
+
+# Folder for template files, if folder exists
 templates_path = []
 if Path(conf_dir, "_templates").exists():
     templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
 
+# -- HTML OUTPUT -----------------------------------------------------------------------
 
-# -- Options for HTML output -------------------------------------------------
+# Add links to *.rst source files on HTML pages
+html_show_sourcelink = True
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# Pygments syntax highlighting style
+pygments_style = "sphinx"
+
 # Use RTD Theme if installed
 try:
     import sphinx_rtd_theme
@@ -80,11 +101,11 @@ else:
     extensions.append("sphinx_rtd_theme")
     html_theme = "sphinx_rtd_theme"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
-if Path(conf_dir, "_static").exists():
-    html_static_path = ["_static"]
+# rst_epilog = """
+# .. |release_date| replace:: {release_date}
+# .. |coverage-equals-release| replace:: coverage=={release}
+# .. |doc-url| replace:: https://coverage.readthedocs.io/en/coverage-{release}
+# .. |br| raw:: html
+#   <br/>
+# """.format(release=release, release_date=release_date)
 
-html_show_sourcelink = True
