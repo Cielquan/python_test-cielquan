f29875f32bc47799ee72d0f3015acf9c5033d458
Cielquan <cielquan@protonmail.com>
2020-02-02 15:06:35 +0100

    test build

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# ==============================================================================
+# Copyright (c) 2020 Christian Riedel
+#
+# This file 'setup.py' created 2020-01-19 is part of the project/program 'python_test'.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the MIT License as published by
+# the Massachusetts Institute of Technology.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MIT License for more details.
+#
+# You should have received a copy of the MIT License
+# along with this program. If not, see <https://opensource.org/licenses/MIT>.
+#
+# Github: https://github.com/Cielquan/
+# ==============================================================================
+"""docstring"""
+import textwrap
+
+import setuptools
+
+
+setuptools.setup(
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+)
