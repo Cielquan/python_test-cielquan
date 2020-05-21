e9fb9341ef2cf963edccc257fd7cd188aaa79ef2
Cielquan <cielquan@protonmail.com>
2020-05-21 11:04:51 +0200

    added pre-com runner script

---
+++ pre_commit_runner.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Script to call local installations of tools from `pre-commit`.
+
+Script considers OS and calls tool accordingly.
+"""
+import subprocess
+import sys
+
+from pathlib import Path
+
+
+def main():
+    """Call `tool` given as first argument from a tox env"""
+    if sys.platform == "win32":
+        exe = Path("Scripts/" + sys.argv[1] + ".exe")
+    else:
+        exe = Path("bin/" + sys.argv[1])
+
+    tox = Path(".tox")
+    envs = ("pre-commit", "dev", "devug")
+
+    cmd = None
+    for env in envs:
+        path = Path(tox / env / exe)
+        if path.is_file():
+            cmd = (str(path), *sys.argv[2:])
+
+    if cmd is None:
+        print(
+            "No 'pylint' executable found. Make sure one of the "
+            "following `tox` envs is accessible: {}".format(envs)
+        )
+        return 1
+
+    return subprocess.call(cmd)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
