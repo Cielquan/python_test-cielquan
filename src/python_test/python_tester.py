8b66dc88d3b1b99fece526cf3b051e98daabfd9e
Cielquan <cielquan@protonmail.com>
2020-06-08 16:52:13 +0200

    added ver stuff to pkg

--- src/python_test/python_tester.py
+++ src/python_test/python_tester.py
@@ -10,8 +10,14 @@ def testus(zahl: int = 2) -> int:
     :param zahl: default: 2
     :return: 2 * zahl
     """
-    __py_min_ver__ = (3, 7)
+    __py36__ = (3, 6)
+    __py37__ = (3, 7)
+    __py38__ = (3, 8)
 
-    if sys.version_info < __py_min_ver__:
+    if sys.version_info == __py36__:
+        return 2 * zahl
+    if sys.version_info == __py37__:
+        return 2 * zahl
+    if sys.version_info == __py38__:
         return 2 * zahl
     return 2 * zahl
