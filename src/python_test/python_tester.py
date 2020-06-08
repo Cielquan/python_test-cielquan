c98e96c0ede8fed994eb4b65421eb754eb47fea8
Cielquan <cielquan@protonmail.com>
2020-06-08 16:55:51 +0200

    added ver stuff to pkg

--- src/python_test/python_tester.py
+++ src/python_test/python_tester.py
@@ -12,12 +12,9 @@ def testus(zahl: int = 2) -> int:
     """
     __py36__ = (3, 6)
     __py37__ = (3, 7)
-    __py38__ = (3, 8)
 
     if sys.version_info == __py36__:
         return 2 * zahl
     if sys.version_info == __py37__:
-        return 2 * zahl
-    if sys.version_info == __py38__:
         return 2 * zahl
     return 2 * zahl
