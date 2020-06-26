0a0fdb01de163118a2ae0e0500c0d6f620ba9051
Christian Riedel <cannotguessit+github@protonmail.com>
2020-06-26 15:56:05 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,25 +16,87 @@ import sys
 from datetime import date
 from importlib.util import find_spec
 from pathlib import Path
-from typing import List, Optional
+from typing import Any, Iterable, List, Union
 
 import sphinx_rtd_theme  # type: ignore[import]
 
-from python_test import __version__  # CHANGE ME
+from dotenv import find_dotenv, load_dotenv
+
+from python_test import __version__
 
 
-sys.path.insert(0, str(Path(__file__).parents[2]))  #: Add Repo to path
 needs_sphinx = "3.0"  #: Minimum Sphinx version to build the docs
+sys.path.insert(0, str(Path(__file__).parents[2]))  #: Add Repo to PATH
 
 
 #: -- GLOB VARS ------------------------------------------------------------------------
 CONF_DIR = Path(__file__)
-YEAR = f"{date.today().year}"
 NOT_LOADED_MSG = []
+YEAR = f"{date.today().year}"
+
+
+#: -- GLOB TYPES -----------------------------------------------------------------------
+EnvVarTypes = Union[str, int, float, bool, None]
 
 
 #: -- UTILS ----------------------------------------------------------------------------
-def get_release_level(version_str: str) -> Optional[str]:
+load_dotenv(find_dotenv())  #: Load .env file from project root
+
+
+def get_env_var(
+    var_name: str,
+    default: Any = None,
+    rv_type: type = str,
+    *,
+    raise_error_if_no_value: bool = False,
+    true_bool_values: Iterable = (1, "y", "yes", "t", True),
+    false_bool_values: Iterable = (0, "n", "no", "f", False),
+) -> EnvVarTypes:
+    """Wrap `os.getenv` to adjust the type of the values.
+
+    :param var_name: Name of the environment variable.
+    :param default: Default value if no value is found for :param var_name:.
+        Default is: `None`.
+    :param rv_type: Type the value of the environment variable should be changed into.
+        Default is: `str`.
+    :param raise_error_if_no_value: If `True` raises an `KeyError` when no value is
+        found for :param var_name: and :param default: is None.
+        Parameter is keyword only and defaults to: `False`.
+    :param true_bool_values: Iterable of objects whose string representations are
+        matched against the environment variable's value if the :param rv_type: is
+        `bool`. If a match is found `True` is returned.
+        Parameter is keyword only and defaults to: (1, "y", "yes", "t", True)
+    :param false_bool_values: Iterable of objects whose string representations are
+        matched against the environment variable's value if the :param rv_type: is
+        `bool`. If a match is found `False` is returned.
+        Parameter is keyword only and defaults to: (0, "n", "no", "f", False)
+    """
+    env_var = os.getenv(var_name, default)
+
+    if not env_var and default is None:
+        if raise_error_if_no_value:
+            raise KeyError(
+                f"Environment variable '{var_name}' not set or empty and no default."
+            ) from None
+        return None
+
+    if isinstance(rv_type, bool):
+        true_bool_values = set(true_bool_values)
+        false_bool_values = set(false_bool_values)
+        if str(env_var).casefold() in (str(b).casefold() for b in true_bool_values):
+            return True
+        if str(env_var).casefold() in (str(b).casefold() for b in false_bool_values):
+            return False
+        raise KeyError(
+            f"Environment variable '{var_name}' has an invalid boolean value.\n"
+            f"For true use any of: {true_bool_values}\n"
+            f"For false use any of: {false_bool_values}"
+        ) from None
+
+    return rv_type(env_var)
+
+
+def get_release_level(version_str: str) -> str:
     """Extract release tag from version string."""
     tag = re.search(r"^[v]?\d+\.\d+\.\d+[+-]?([a-zA-Z]*)\d*", version_str)
     if tag:
@@ -157,6 +219,25 @@ else:
     NOT_LOADED_MSG.append(
         "## 'sphinx-click' extension not loaded - extension or 'click' not installed"
     )
+
+
+#: -- CONFLUENCE BUILDER ---------------------------------------------------------------
+#: needs install: "sphinxcontrib-confluencebuilder"
+extensions.append("sphinxcontrib.confluencebuilder")
+confluence_publish = True
+confluence_server_url = "https://siegwerk.atlassian.net/wiki/"
+confluence_server_user = get_env_var("CONFLUENCE_SERVER_USER")
+confluence_server_pass = get_env_var("CONFLUENCE_SERVER_PASS")
+confluence_space_name = "SWFPTOOL"
+confluence_parent_page = "SPHINXTEST"  # "SPHINXTEST"
+confluence_page_hierarchy = True
+confluence_prev_next_buttons_location = "bottom"
+confluence_timeout = 30
+confluence_purge = True
+# confluence_publish_postfix = "-test-0"  # TODO: remove
+
+if tags.has("builder_confluence"):
+    extensions.remove("sphinx.ext.viewcode")
 
 
 #: -- HTML THEME -----------------------------------------------------------------------
