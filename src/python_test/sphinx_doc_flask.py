8c5231f4f5e6cc1a571a9e26503a4ad75e16b7f6
Christian Riedel <cielquan@protonmail.com>
2020-08-11 11:53:04 +0200

    Update sphinx_doc_flask.py

--- src/python_test/sphinx_doc_flask.py
+++ src/python_test/sphinx_doc_flask.py
@@ -1,16 +1,23 @@
+"""Serve sphinx docs."""
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
-@app.route('/docs/', defaults={'filename': 'index.html'})
-@app.route('/docs/<path:filename>')
+@app.route("/docs/", defaults={"filename": "index.html"})
+@app.route("/docs/<path:filename>")
 def web_docs(filename):
-    app.static_folder = 'static/docs'
+    """Route the given doc page.
+
+    :param filename: File name from URL
+    :return: Doc page
+    """
+    app.static_folder = "static/docs"
     rv = app.send_static_file(filename)
-    app.static_folder = 'static'
+    app.static_folder = "static"
     return rv
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)  # nosec
