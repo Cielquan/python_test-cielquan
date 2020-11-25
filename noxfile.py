9ca0b2bca3865e778048a7b96d8050e0c6c5e505
Cielquan <cielquan@protonmail.com>
2020-11-25 23:32:40 +0100

    WIP tox calls nox

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,16 @@
 """Config file for nox."""
-# TODO: check why nox test let cov fail and tox test not
+# TODO: add nox env to call full test+cov on tox
+# TODO: add nox env to call full doc test on tox
+# TODO: tox create env and for cmd calls nox
+# TODO: nox installs only if not called by tox
+# TODO: nox sessions have no venv
 import contextlib
 import re
 import subprocess  # noqa: S404
 import sys
 
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Mapping, Optional, Set
+from typing import Any, Callable, Dict, Optional
 
 import nox
 import nox.command
@@ -18,6 +22,7 @@ from tomlkit import parse  # type: ignore[import]
 
 #: -- MANUAL CONFIG --------------------------------------------------------------------
 #: Config  # CHANGE ME
+# TODO: grab from "tox -va"
 PYTHON_TEST_VERSIONS = ["3.6", "3.7", "3.8", "3.9", "3.10", "pypy3"]
 SPHINX_BUILDERS = ["html", "linkcheck", "coverage", "doctest", "confluence"]
 
@@ -44,186 +49,17 @@ COV_CACHE_DIR = NOXFILE_DIR / ".coverage_cache"
 JUNIT_CACHE_DIR = NOXFILE_DIR / ".junit_cache"
 PACKAGE_NAME = str(PYPROJECT["tool"]["poetry"]["name"])
 
-TOX_CODE_TEST_VERSION_FORMAT = (
-    "{"
-    + ",".join([v.replace(".", "").replace("pypy", "py") for v in PYTHON_TEST_VERSIONS])
-    + "}"
-)
-COVERAGE_FILE_TOX_EXPRESSION = (
-    "{env:COVERAGE_FILE:{toxinidir}/.coverage_cache/.coverage.{envname}}"
-)
-TOX_INI_FILE = f"""[tox]
-minversion = 3.15.0
-isolated_build = true
-skipsdist = false
-
-[testenv:py{TOX_CODE_TEST_VERSION_FORMAT}]
-description = run tests with {"{basepython}"}
-passenv =
-    HOME
-    CI
-    PYTEST_*
-setenv =
-    PIP_DISABLE_VERSION_CHECK = 1
-    COVERAGE_FILE = {COVERAGE_FILE_TOX_EXPRESSION}
-download = true
-extras = testing
-commands =
-    pytest \\
-    --basetemp={"{envtmpdir}"} \\
-    --junitxml={"{toxinidir}"}/.junit_cache/junit.{"{envname}"}.xml \\
-    --cov={"{envsitepackagesdir}"}/{PACKAGE_NAME} \\
-    --cov-fail-under=0 \\
-    --numprocesses={"{env:PYTEST_XDIST_N:auto}"} \\
-    {"{posargs:tests}"}
-"""
-
 
 #: -- MONKEYPATCH SESSION --------------------------------------------------------------
 class Session(_Session):  # noqa: R0903
     """Subclass of nox's Session class to add `poetry_install` method."""
-
-    def __init__(self, runner: nox.sessions.SessionRunner) -> None:
-        """Overwrite method to add passenv attribute."""
-        super().__init__(runner)
-        self.passenv: List[str] = []
-
-    def setenv(self, env_vars: Dict[str, str]) -> None:
-        """Set given envvars and add them to passenv."""
-        for envvar in env_vars:
-            self.passenv.append(envvar)
-            self.env[envvar] = env_vars[envvar]
-
-    def _run(
-        self,
-        *args: str,
-        env: Mapping[str, str] = None,
-        **kwargs: Any,
-    ) -> Any:
-        """Overwrite method to add env filter functionallity.
-
-        Additional kwargs:
-        :param filter_env: bool if env should be filtered
-        :param passenv: list of envvars to not filter out
-        """
-        # Legacy support - run a function given.
-        if callable(args[0]):
-            return self._run_func(args[0], args[1:], kwargs)
-
-        # Combine the env argument with our virtualenv's env vars.
-        run_env = self.env.copy()
-        if env is not None:
-            run_env.update(env)
-
-        # Filter env
-        if kwargs.pop("filter_env", False):
-            run_env = self._filter_env(run_env, env, kwargs.pop("passenv", None))
-
-        # If --error-on-external-run is specified, error on external programs.
-        if self._runner.global_config.error_on_external_run:
-            kwargs.setdefault("external", "error")
-
-        # Allow all external programs when running outside a sandbox.
-        if not self.virtualenv.is_sandboxed:
-            kwargs["external"] = True
-
-        if args[0] in self.virtualenv.allowed_globals:
-            kwargs["external"] = True
-
-        # Run a shell command.
-        return nox.command.run(args, env=run_env, paths=self.bin_paths, **kwargs)
-
-    def _filter_env(
-        self,
-        run_env: Dict[str, str],
-        env: Optional[Mapping[str, str]] = None,
-        kwargs_passenv: Optional[List[str]] = None,
-    ) -> Dict[str, str]:
-        """Filter given environment.
-
-        :param run_env: Environment to filter
-        :param env: Env added for single command; defaults to None
-        :param kwargs_passenv: Passenv added for single command; defaults to None
-        :return: Filtered environment
-        """
-        kwargs_passenv_wo_asterisk = set("")
-        kwargs_passenv_regex = set("")
-        if kwargs_passenv:
-            kwargs_passenv_wo_asterisk = {v for v in kwargs_passenv if "*" not in v}
-            kwargs_passenv_regex = {v for v in kwargs_passenv if "*" in v}
-
-        self_passenv_wo_asterisk = set("")
-        self_passenv_regex = set("")
-        if self.passenv:
-            self_passenv_wo_asterisk = {v for v in self.passenv if "*" not in v}
-            self_passenv_regex = {v for v in self.passenv if "*" in v}
-
-        passenv_regex = re.compile(
-            "|".join(
-                v.replace("*", ".*")
-                for v in (kwargs_passenv_regex | self_passenv_regex)
-            )
-        )
-
-        return {
-            var: run_env[var]
-            for var in run_env
-            if var
-            in self._create_envvar_whitelist(
-                set(env or "") | kwargs_passenv_wo_asterisk | self_passenv_wo_asterisk
-            )
-            or passenv_regex.fullmatch(var)
-        }
-
-    @staticmethod
-    def _create_envvar_whitelist(whitelist: Optional[Set[str]] = None) -> Set[str]:
-        """Merge ENVVAR whitelists.
-
-        Based on ``tox.config.__init__.tox_addoption.passenv()``.
-        """
-        nox_whitelist = {"_"}
-
-        tox_whitelist = {
-            "CURL_CA_BUNDLE",
-            "LANG",
-            "LANGUAGE",
-            "LD_LIBRARY_PATH",
-            "PATH",
-            "PIP_INDEX_URL",
-            "PIP_EXTRA_INDEX_URL",
-            "REQUESTS_CA_BUNDLE",
-            "SSL_CERT_FILE",
-            "HTTP_PROXY",
-            "HTTPS_PROXY",
-            "NO_PROXY",
-        }
-
-        if sys.platform == "win32":
-            os_whitelist = {
-                "SYSTEMDRIVE",  # needed for pip6
-                "SYSTEMROOT",  # needed for python's crypto module
-                "PATHEXT",  # needed for discovering executables
-                "COMSPEC",  # needed for distutils cygwincompiler
-                "TEMP",
-                "TMP",
-                # for `multiprocessing.cpu_count()` on Windows (prior to Python 3.4).
-                "NUMBER_OF_PROCESSORS",
-                "PROCESSOR_ARCHITECTURE",  # platform.machine()
-                "USERPROFILE",  # needed for `os.path.expanduser()`
-                "MSYSTEM",  # fixes tox#429
-            }
-        else:
-            os_whitelist = {"TMPDIR"}
-
-        custom_whitelist = set() if whitelist is None else whitelist
-
-        return nox_whitelist | tox_whitelist | os_whitelist | custom_whitelist
 
     def poetry_install(
         self,
         extras: Optional[str] = None,
         no_dev: bool = True,
         no_root: bool = False,
+        require_venv: bool = False,
         **kwargs: Any,
     ) -> None:
         """Wrap `poetry install` for nox sessions.
@@ -245,19 +81,21 @@ class Session(_Session):  # noqa: R0903
                 "A session without a virtualenv can not install dependencies."
             )
 
-        self.install("poetry>=1", env={"PIP_DISABLE_VERSION_CHECK": "1"})
+        _env = {"PIP_DISABLE_VERSION_CHECK": "1"}
+        _req_venv = {"PIP_REQUIRE_VIRTUALENV": "true"}
 
-        extra_deps = []
-        if extras:
-            extra_deps = ["--extras", extras]
+        if require_venv or isinstance(self.virtualenv, nox.sessions.PassthroughEnv):
+            _env.update(_req_venv)
+            if "env" in kwargs:
+                kwargs["env"].update(_req_venv)
+            else:
+                kwargs["env"] = _req_venv
 
-        no_dev_flag = []
-        if no_dev:
-            no_dev_flag = ["--no-dev"]
+        self.install("poetry>=1", env=_env)
 
-        no_root_flag = []
-        if no_root:
-            no_root_flag = ["--no-root"]
+        extra_deps = ["--extras", extras] if extras else []
+        no_dev_flag = ["--no-dev"] if no_dev else []
+        no_root_flag = ["--no-root"] if no_root else []
 
         self._run(
             "poetry", "install", *no_root_flag, *no_dev_flag, *extra_deps, **kwargs
@@ -313,7 +151,7 @@ def safety(session: Session) -> None:
         )
 
     session.run(
-        "safety", "check", "-r", str(req_file_path), "--full-report", filter_env=True
+        "safety", "check", "-r", str(req_file_path), "--full-report",
     )
 
 
@@ -335,8 +173,6 @@ def pre_commit(session: Session) -> None:
     if not hooks:
         hooks.append("")
 
-    session.passenv = ["SSH_AUTH_SOCK", "SKIP"]
-
     for hook in hooks:
         add_args = show_diff + [hook]
         session.run(
@@ -345,7 +181,7 @@ def pre_commit(session: Session) -> None:
             "--all-files",
             "--color=always",
             *add_args,
-            filter_env=True,
+    
         )
 
     bin_dir = session.bin
@@ -362,8 +198,8 @@ def package(session: Session) -> None:
     """Check sdist and wheel."""
     session.poetry_install("poetry twine", no_root=True)
 
-    session.run("poetry", "build", "-vvv", filter_env=True)
-    session.run("twine", "check", "dist/*", filter_env=True)
+    session.run("poetry", "build", "-vvv")
+    session.run("twine", "check", "dist/*")
 
 
 @nox.session(python=PYTHON_TEST_VERSIONS)
@@ -391,8 +227,6 @@ def code_test(session: Session) -> None:
         "--cov-fail-under=0",
         f"--numprocesses={session.env.get('PYTEST_XDIST_N') or 'auto'}",
         f"{session.posargs or 'tests'}",
-        filter_env=True,
-        passenv=["COVERAGE_FILE"],
     )
 
 
@@ -424,22 +258,18 @@ def coverage(session: Session) -> None:
     session.poetry_install(extras, no_root=True)
 
     if "merge_only" in session.posargs or not session.posargs:
-        session.run("coverage", "combine", filter_env=True, passenv=["COVERAGE_FILE"])
+        session.run("coverage", "combine")
         session.run(
             "coverage",
             "xml",
             "-o",
             f"{COV_CACHE_DIR / 'coverage.xml'}",
-            filter_env=True,
-            passenv=["COVERAGE_FILE"],
         )
         session.run(
             "coverage",
             "html",
             "-d",
             f"{COV_CACHE_DIR / 'htmlcov'}",
-            filter_env=True,
-            passenv=["COVERAGE_FILE"],
         )
 
     if "report_only" in session.posargs or not session.posargs:
@@ -448,8 +278,6 @@ def coverage(session: Session) -> None:
             "report",
             "-m",
             f"--fail-under={session.env.get('MIN_COVERAGE') or 100}",
-            filter_env=True,
-            passenv=["COVERAGE_FILE"],
         )
         session.run(
             "diff-cover",
@@ -459,48 +287,41 @@ def coverage(session: Session) -> None:
             f"--fail-under={session.env.get('MIN_DIFF_COVERAGE') or 100}",
             f"--diff-range-notation={session.env.get('DIFF_RANGE_NOTATION') or '..'}",
             f"{COV_CACHE_DIR / 'coverage.xml'}",
-            filter_env=True,
-            passenv=["COVERAGE_FILE"],
         )
 
 
-@nox.session()
+@nox.session(venv_backend="none")
 @monkeypatch_session
 def docs(session: Session) -> None:
     """Build docs with sphinx."""
-    extras = "docs"
+    extras = ""
+
+    if not "called_by_tox" in session.posargs:
+        extras += " docs"
+
     cmd = "sphinx-build"
-    add_args = []
+    args = ["-b", "html", "-aE", "docs/source", "docs/build/html"]
 
     if "autobuild" in session.posargs or "ab" in session.posargs:
         extras += " sphinx-autobuild"
         cmd = "sphinx-autobuild"
-        add_args = ["--open-browser"]
+        args += ["--open-browser"]
 
-    session.poetry_install(extras)
+    session.poetry_install(extras.strip())
 
-    session.run(
-        cmd,
-        "-b",
-        "html",
-        "-aE",
-        "docs/source",
-        "docs/build/html",
-        *add_args,
-        filter_env=True,
-    )
+    session.run(cmd, *args)
 
-    if not session.posargs:
-        index_file = Path(NOXFILE_DIR) / "docs/build/html/index.html"
-        print(f"DOCUMENTATION AVAILABLE UNDER: {index_file.as_uri()}")
+    index_file = Path(NOXFILE_DIR) / "docs/build/html/index.html"
+    print(f"DOCUMENTATION AVAILABLE UNDER: {index_file.as_uri()}")
 
 
 @nox.parametrize("builder", SPHINX_BUILDERS)
-@nox.session()
+@nox.session(venv_backend="none")
 @monkeypatch_session
 def docs_test(session: Session, builder: str) -> None:
     """Build and check docs with (see env name) sphinx builder."""
-    session.poetry_install("docs")
+    if not "called_by_tox" in session.posargs:
+        session.poetry_install("docs")
 
     session.run(
         "sphinx-build",
@@ -513,7 +334,7 @@ def docs_test(session: Session, builder: str) -> None:
         "docs/source",
         f"docs/build/test/{builder}",
         *(["-t", "builder_confluence"] if builder == "confluence" else []),
-        filter_env=True,
+
     )
 
 
@@ -535,7 +356,7 @@ def poetry_install_all_extras(session: Session) -> None:
 
     session.poetry_install(install_extras, no_dev=False)
 
-    session.run("python", "-m", "pip", "list", "--format=columns", filter_env=True)
+    session.run("python", "-m", "pip", "list", "--format=columns")
     print(f"PYTHON INTERPRETER LOCATION: {sys.executable}")
 
 
