958360907156daa9d363b4720dca93989cbc87ce
Christian Riedel <cielquan@protonmail.com>
2020-11-27 19:32:14 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 from datetime import date
 from importlib.util import find_spec
 from pathlib import Path
-from typing import Any, List, Union
+from typing import Any, List
 
 import sphinx_rtd_theme  # type: ignore[import]
 import tomlkit  # type: ignore[import]
