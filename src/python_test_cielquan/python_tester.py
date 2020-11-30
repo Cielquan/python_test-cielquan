e7084dfcfa8dbc54c2934f26aa0bf527b39ae9f2
Christian Riedel <cielquan@protonmail.com>
2020-11-30 13:02:58 +0100

    Update python_tester.py

--- src/python_test_cielquan/python_tester.py
+++ src/python_test_cielquan/python_tester.py
@@ -12,10 +12,10 @@ def some_func(number: int = 2) -> int:
     __py36__ = (3, 6)
     __py37__ = (3, 7)
 
-    if sys.version_info[0:2] == __py36__:  # pragma: py36
-        return 2 * number
-    if sys.version_info[0:2] == __py37__:  # pragma: py37
-        return 2 * number
+    if sys.version_info[0:2] == __py36__:  # pragma: py-gte-36
+        return 2 * number  # pragma: py36
+    if sys.version_info[0:2] == __py37__:  # pragma: py-gte-37
+        return 2 * number  # pragma: py37
     return 2 * number  # pragma: py-gte-38
 
 
