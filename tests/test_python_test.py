38047d5e98b3efc0dd91490ee3ea1cdb5b4d8836
Christian Riedel <cielquan@protonmail.com>
2020-11-30 11:16:52 +0100

    Update test_python_test.py

--- tests/test_python_test.py
+++ tests/test_python_test.py
@@ -1,14 +1,14 @@
 """docstring."""
 import python_test_cielquan
 
-from python_test_cielquan.python_tester import test_us
+from python_test_cielquan.python_tester import some_func
 
 
 def test_something() -> None:
     """docstring."""
     test_val = 1
 
-    result = test_us(test_val)
+    result = some_func(test_val)
 
     assert result == 2
 
