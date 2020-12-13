2e8a15af328fc3eadfff581e1e6a3308b691e1aa
Cielquan <cielquan@protonmail.com>
2020-12-13 17:59:17 +0100

    sv conf

--- .versionrc.js
+++ .versionrc.js
@@ -5,5 +5,15 @@ const tracker = {
 
 module.exports = {
   bumpFiles: [tracker],
-  packageFiles: [tracker]
+  packageFiles: [tracker],
+  types: [
+    {"type": "feat", "section": "Features"},
+    {"type": "fix", "section": "Bug Fixes"},
+    {"type": "docs", "section": "Documentation"},
+    {"type": "chore", "hidden": true},
+    {"type": "style", "hidden": true},
+    {"type": "refactor", "hidden": true},
+    {"type": "perf", "hidden": true},
+    {"type": "test", "hidden": true}
+  ]
 }
