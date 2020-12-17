b5974c30cd5c0896e423d3154cbaf42d9fe47b0b
Christian Riedel <cielquan@protonmail.com>
2020-12-17 11:45:19 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,12 @@
-#!/usr/bin/env python3
+"""
+    tests.conftest
+    ~~~~~~~~~~~~~~
 
-# ==============================================================================
-# Copyright (c) 2020 Christian Riedel
-#
-# This file 'conftest.py' created 2020-01-19
-# is part of the project/program 'python_test'.
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the MIT License as published by
-# the Massachusetts Institute of Technology.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# MIT License for more details.
-#
-# You should have received a copy of the MIT License
-# along with this program. If not, see <https://opensource.org/licenses/MIT>.
-#
-# Github: https://github.com/Cielquan/
-# ==============================================================================
-"""docstring."""
+    Init file for test suit.
+
+    :copyright: (c) Christian Riedel
+    :license: MIT, see LICENSE for more details
+"""  # noqa: D205,D208,D400
 import pytest
 
 
