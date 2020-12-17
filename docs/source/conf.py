03ae14062e38d78498fb3876cd5c72ef80ddacb3
Christian Riedel <cielquan@protonmail.com>
2020-12-17 12:01:06 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ copyright = (  # noqa: VNE003
 )
 release = __version__  #: The full version, including alpha/beta/rc tags
 version_parts = re.search(
-    r"^v(?P<version>\d+\.\d+)\.\d+[-.]?(?P<tag>[a-z]*)[\.]?\d*", __version__
+    r"^v?(?P<version>\d+\.\d+)\.\d+[-.]?(?P<tag>[a-z]*)[\.]?\d*", __version__
 )
 version = (
     None if not version_parts else version_parts.group("version")
