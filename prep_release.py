c1f57de51f45f3fa8e7c5c03d04971f39e8e6d37
Cielquan <cielquan@protonmail.com>
2021-01-15 15:27:08 +0100

    finish prep_release.py

--- prep_release.py
+++ prep_release.py
@@ -11,6 +11,7 @@
     :copyright: (c) 2019-2020, Christian Riedel
     :license: GPL-3.0, see LICENSE for details
 """  # noqa: D205,D208,D400
+import argparse
 import re
 import subprocess  # noqa: S404
 import sys
@@ -18,6 +19,15 @@ import sys
 from datetime import date
 
 import tomlkit  # type: ignore[import]
+
+
+if sys.version_info[0:2] <= (3, 6):
+    raise Exception
+
+
+PATCH = ("patch",)
+MINOR = ("minor", "feature")
+MAJOR = ("major", "breaking")
 
 
 #: -- UTILS ----------------------------------------------------------------------------
@@ -66,50 +76,54 @@ def bump_version(release_type: str = "patch") -> str:
     :raises ValueError: when the version string from pyproject.toml is not parsable.
     :return: new version string
     """
-    patch = ("patch",)
-    minor = ("minor", "feature")
-    major = ("major", "breaking")
-
-    if release_type not in patch + minor + major:
+    if release_type not in PATCH + MINOR + MAJOR:
         raise ValueError(f"Invalid version increase type: {release_type}")
 
     pyproject_config = _get_pyproject_config()
-    version = pyproject_config["tool"]["poetry"]["version"]
+    current_version = pyproject_config["tool"]["poetry"]["version"]
 
-    version_parts = re.match(r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)", version)
+    version_parts = re.match(
+        r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)", current_version
+    )
     if not version_parts:
-        raise ValueError(f"Unparsable version: {version}")
+        raise ValueError(f"Unparsable version: {current_version}")
 
-    if release_type in major:
+    if release_type in MAJOR:
         version = (
             f"{int(version_parts.group('major')) + 1}"
             f".{version_parts.group('minor')}"
             f".{version_parts.group('patch')}"
         )
-    elif release_type in minor:
+    elif release_type in MINOR:
         version = (
             f"{version_parts.group('major')}"
             f".{int(version_parts.group('minor')) + 1}"
             f".{version_parts.group('patch')}"
         )
-    elif release_type in patch:
+    elif release_type in PATCH:
         version = (
             f"{version_parts.group('major')}"
             f".{version_parts.group('minor')}"
             f".{int(version_parts.group('patch')) + 1}"
         )
+    else:
+        print("Given `RELEASE TYPE` is invalid.")
+        sys.exit(1)
 
     pyproject_config["tool"]["poetry"]["version"] = version
     _set_pyproject_config(pyproject_config)
     return version
 
 
-def update_changelog(new_version: str, last_version: str, repo_url: str) -> None:
+def update_changelog(
+    new_version: str, last_version: str, repo_url: str, first_release: bool
+) -> None:
     """Update CHANGELOG.md to be release ready.
 
     :param new_version: new version string
     :param last_version: current version string
     :param repo_url: URL to source code at GitHub
+    :first_release: if this is the first release
     """
     with open("CHANGELOG.md") as changelog_file:
         changelog_lines = changelog_file.read().split("\n")
@@ -125,21 +139,22 @@ def update_changelog(new_version: str, last_version: str, repo_url: str) -> None
         changelog_lines[release_line] = (
             f"## Unreleased - [diff]({repo_url}/compare/v{new_version}...master)\n\n\n"
             f"## [{new_version}]({repo_url}/releases/v{new_version}) ({today})"
-            f" - [diff]({repo_url}/compare/v{last_version}...v{new_version})"
+            f" - [diff]({repo_url}/compare/"
+            f"{'' if first_release else 'v'}{last_version}...v{new_version})"
         )
 
     with open("CHANGELOG.md", "w") as changelog_file:
         changelog_file.write("\n".join(changelog_lines))
 
 
-def commit_and_tag(new_version: str) -> None:
+def commit_and_tag(version: str) -> None:
     """Git commit and tag the new release."""
     subprocess.run(  # noqa: S603,S607
         [
             "git",
             "commit",
             "--no-verify",
-            f"--message='release v{new_version}'",
+            f"--message='release v{version}'",
             "--include",
             "pyproject.toml",
             "CHANGELOG.md",
@@ -147,21 +162,51 @@ def commit_and_tag(new_version: str) -> None:
         check=True,
     )
     subprocess.run(  # noqa: S603,S607
-        ["git", "tag", "-am", f"'v{new_version}'", f"v{new_version}"], check=True
+        ["git", "tag", "-am", f"'v{version}'", f"v{version}"], check=True
     )
 
 
-if __name__ == "__main__":
-    if "--first-release" in sys.argv:
-        sys.argv.remove("--first-release")
-        VERSION = _get_current_version()
-        CURRENT_VERSION = str(
-            subprocess.run(  # noqa: S603,S607
-                ["git", "rev-list", "--max-parents=0", "HEAD"], check=True
-            )
-        )[0:7]
+def _parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "increase_type",
+        metavar="RELEASE TYPE",
+        default="patch",
+        nargs="?",
+        help=(
+            "Release type: patch | minor/feature | major/breaking; "
+            "gets ignored on `--first-release`; "
+            "defaults to patch"
+        ),
+    )
+    parser.add_argument(
+        "--first-release",
+        action="store_true",
+        help="Flag for first release to prevent version bumping.",
+    )
+    return parser.parse_args()
+
+
+def _main() -> int:
+    args = _parser()
+
+    if args.first_release:
+        release_version = _get_current_version()
+        #: Get first commit
+        current_version = subprocess.run(  # noqa: S603,S607
+            ["git", "rev-list", "--max-parents=0", "HEAD"],
+            check=True,
+            capture_output=True,
+        ).stdout.decode()[0:7]
     else:
-        VERSION = bump_version("patch" if len(sys.argv) <= 1 else sys.argv[1])
-        CURRENT_VERSION = _get_current_version()
-    update_changelog(VERSION, CURRENT_VERSION, _get_repo_url())
-    commit_and_tag(VERSION)
+        release_version = bump_version(args.increase_type)
+        current_version = _get_current_version()
+    update_changelog(
+        release_version, current_version, _get_repo_url(), args.first_release
+    )
+    commit_and_tag(release_version)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(_main())
