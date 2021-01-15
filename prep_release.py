da4746af1432e692ca3f7ac380440b6de35e0c9f
Christian Riedel <cannotguessit+github@protonmail.com>
2021-01-15 08:22:23 +0100

    Update prep_release.py

--- prep_release.py
+++ prep_release.py
@@ -13,13 +13,15 @@
 """  # noqa: D205,D208,D400
 import re
 import subprocess  # noqa: S404
+import sys
 
 from datetime import date
 
 import tomlkit  # type: ignore[import]
 
 
-def get_pyproject_config() -> tomlkit.toml_document.TOMLDocument:
+#: -- UTILS ----------------------------------------------------------------------------
+def _get_pyproject_config() -> tomlkit.toml_document.TOMLDocument:
     """Load config from pyproject.toml file.
 
     :return: config as dict-like object
@@ -28,7 +30,7 @@ def get_pyproject_config() -> tomlkit.toml_document.TOMLDocument:
         return tomlkit.parse(pyproject_file.read())
 
 
-def set_pyproject_config(pyproject_config: tomlkit.toml_document.TOMLDocument) -> None:
+def _set_pyproject_config(pyproject_config: tomlkit.toml_document.TOMLDocument) -> None:
     """Write given config to pyproect.toml file.
 
     :param pyproject_config: config to write
@@ -37,20 +39,69 @@ def set_pyproject_config(pyproject_config: tomlkit.toml_document.TOMLDocument) -
         pyproject_file.write(tomlkit.dumps(pyproject_config))
 
 
-def get_current_version() -> str:
+def _get_current_version() -> str:
     """Extract the version from pyproject.toml file.
 
     :return: version string
     """
-    return str(get_pyproject_config()["tool"]["poetry"]["version"])
+    return str(_get_pyproject_config()["tool"]["poetry"]["version"])
 
 
-def get_repo_url() -> str:
+def _get_repo_url() -> str:
     """Extract source code URL at GitHub from pyproject.toml file.
 
     :return: URL
     """
-    return str(get_pyproject_config()["tool"]["poetry"]["urls"]["Source"])
+    return str(_get_pyproject_config()["tool"]["poetry"]["urls"]["Source"])
+
+
+#: -- MAIN -----------------------------------------------------------------------------
+def bump_version(release_type: str = "patch") -> str:
+    """Bump the current version for the next release.
+
+    :param release_type: type of release;
+        allowed values are: patch | minor/feature | major/breaking;
+        defaults to "patch"
+    :raises ValueError: when an invalid release_type is given.
+    :raises ValueError: when the version string from pyproject.toml is not parsable.
+    :return: new version string
+    """
+    patch = ("patch",)
+    minor = ("minor", "feature")
+    major = ("major", "breaking")
+
+    if release_type not in patch + minor + major:
+        raise ValueError(f"Invalid version increase type: {release_type}")
+
+    pyproject_config = _get_pyproject_config()
+    version = pyproject_config["tool"]["poetry"]["version"]
+
+    version_parts = re.match(r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)", version)
+    if not version_parts:
+        raise ValueError(f"Unparsable version: {version}")
+
+    if release_type in major:
+        version = (
+            f"{int(version_parts.group('major')) + 1}"
+            f".{version_parts.group('minor')}"
+            f".{version_parts.group('patch')}"
+        )
+    elif release_type in minor:
+        version = (
+            f"{version_parts.group('major')}"
+            f".{int(version_parts.group('minor')) + 1}"
+            f".{version_parts.group('patch')}"
+        )
+    elif release_type in patch:
+        version = (
+            f"{version_parts.group('major')}"
+            f".{version_parts.group('minor')}"
+            f".{int(version_parts.group('patch')) + 1}"
+        )
+
+    pyproject_config["tool"]["poetry"]["version"] = version
+    _set_pyproject_config(pyproject_config)
+    return version
 
 
 def update_changelog(new_version: str, last_version: str, repo_url: str) -> None:
@@ -81,56 +132,19 @@ def update_changelog(new_version: str, last_version: str, repo_url: str) -> None
         changelog_file.write("\n".join(changelog_lines))
 
 
-def bump_version(release_type: str = "patch") -> None:
-    """Bump the current version for the next release.
-
-    :param release_type: type of release;
-        allowed values are: patch | minor/feature | major/breaking;
-        defaults to "patch"
-    :raises ValueError: when an invalid release_type is given.
-    :raises ValueError: when the version string from pyproject.toml is not parsable.
-    """
-    patch = tuple("patch")
-    minor = ("minor", "feature")
-    major = ("major", "breaking")
-
-    if release_type not in patch + minor + major:
-        raise ValueError(f"Invalid version increase type: {release_type}")
-
-    pyproject_config = get_pyproject_config()
-    version = pyproject_config["tool"]["poetry"]["version"]
-
-    version_parts = re.match(r"(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)", version)
-    if not version_parts:
-        raise ValueError(f"Unparsable version: {version}")
-
-    if release_type in patch:
-        version = (
-            f"{version_parts.group('major') + 1}"
-            f".{version_parts.group('minor')}"
-            f".{version_parts.group('patch')}"
-        )
-    elif release_type in minor:
-        version = (
-            f"{version_parts.group('major')}"
-            f".{version_parts.group('minor') + 1}"
-            f".{version_parts.group('patch')}"
-        )
-    elif release_type in major:
-        version = (
-            f"{version_parts.group('major')}"
-            f".{version_parts.group('minor')}"
-            f".{version_parts.group('patch') + 1}"
-        )
-
-    pyproject_config["tool"]["poetry"]["version"] = version
-    set_pyproject_config(pyproject_config)
-
-
 def commit_and_tag(new_version: str) -> None:
     """Git commit and tag the new release."""
     subprocess.run(  # noqa: S603,S607
-        ["git", "commit", "-m", f"'release v{new_version}'"], check=True
+        [
+            "git",
+            "commit",
+            "--no-verify",
+            f"--message='release v{new_version}'",
+            "--include",
+            "pyproject.toml",
+            "CHANGELOG.md",
+        ],
+        check=True,
     )
     subprocess.run(  # noqa: S603,S607
         ["git", "tag", "-am", f"'v{new_version}'", f"v{new_version}"], check=True
@@ -138,4 +152,6 @@ def commit_and_tag(new_version: str) -> None:
 
 
 if __name__ == "__main__":
-    update_changelog("1.0.0", get_current_version(), get_repo_url())
+    bumped_version = bump_version("patch" if len(sys.argv) <= 1 else sys.argv[1])
+    update_changelog(bumped_version, _get_current_version(), _get_repo_url())
+    commit_and_tag(bumped_version)
