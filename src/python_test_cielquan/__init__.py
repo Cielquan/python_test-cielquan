345bc89cce2a96434a7f3873cc69071a5ea5e72d
Christian Riedel <cielquan@protonmail.com>
2021-01-18 13:27:51 +0100

    fix link problem in init

--- src/python_test_cielquan/__init__.py
+++ src/python_test_cielquan/__init__.py
@@ -13,7 +13,7 @@ except ModuleNotFoundError:  # pragma: py-gte-38
     from importlib_metadata import metadata  # type: ignore[import,no-redef]
 
 
-md = dict(metadata(__name__))
+md = metadata(__name__)
 
 
 __author__ = md["Author"]
@@ -22,8 +22,18 @@ __project__ = md["Name"]
 __version__ = md["Version"]
 version_info = tuple(__version__.split("."))
 
-__gh_repository_link__ = md["Project-URL"].replace("Repository, ", "").rstrip("/")
-__gh_repository__ = __gh_repository_link__.replace("https://github.com/", "")
+#: Extract Project-URLs from metadata
+urls = [line[13:] for line in str(md).split("\n") if line.startswith("Project-URL")]
+url_map = {url[:url.find(",")]: url[url.find("http"):] for url in urls}
+source_cat = ("Github", "Repository", "Source", "Code", "Homepage")
 
-# FIXME: fix md to give right url
-__gh_repository_link__ = "https://github.com/Cielquan/python_test-cielquan"
+#: Search for and set a link to GH repo
+__gh_repository_link__ = ""
+for cat in source_cat:
+    if cat in url_map:
+        __gh_repository_link__ = url_map[cat].rstrip("/")
+        __gh_repository__ = __gh_repository_link__.replace("https://github.com/", "")
+        break
+
+if not __gh_repository_link__:
+    raise AttributeError("Metadata do not contain a link to source.")
